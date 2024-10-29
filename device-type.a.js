3aec32742a9a535417514b271e5d920afcd4f578
aungkyawhein <akh.infotech@gmail.com>
2024-10-29 17:17:27 +1300

    update

--- device-type.a.js
+++ device-type.a.js
@@ -2,11 +2,17 @@ const DeviceInfoA = () => {
   const userAgent = navigator.userAgent;
   let deviceType = "Desktop";
   if (/iPad|Android/i.test(userAgent) && !/Mobile/i.test(userAgent)) {
-      deviceType = "Tablet";
+    deviceType = "Tablet";
   } else if (/iPhone|iPod/i.test(userAgent)) {
-      deviceType = "Mobile";
+    deviceType = "Mobile";
   } else if (/Android/i.test(userAgent)) {
-      deviceType = "Mobile";
+    deviceType = "Mobile";
+  }
+
+  if (DeviceInfo.browser === 'Safari' && deviceType === 'Desktop') {
+    if (navigator.maxTouchPoints > 0) {
+      deviceType = 'Tablet';
+    }
   }
 
   return deviceType;
